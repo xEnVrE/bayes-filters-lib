5aaac1ff193551a702044275401fde315294a672
Nicola Piga <nicolapiga@gmail.com>
2020-02-19 11:35:18 +0100

    Implement class VectorDescription

---
+++ src/BayesFilters/include/BayesFilters/VectorDescription.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2016-2019 Istituto Italiano di Tecnologia (IIT)
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD 3-Clause license. See the accompanying LICENSE file for details.
+ */
+
+#ifndef VECTORDESCRIPTION_H
+#define VECTORDESCRIPTION_H
+
+#include <cstddef>
+
+namespace bfl {
+    class VectorDescription;
+}
+
+
+class bfl::VectorDescription
+{
+public:
+    enum class CircularType { Euler, Quaternion };
+
+    VectorDescription() = default;
+
+    VectorDescription(const std::size_t linear_components, const std::size_t circular_components);
+
+    VectorDescription(const std::size_t linear_components, const std::size_t circular_components, const CircularType& circular_type);
+
+    ~VectorDescription() = default;
+
+    std::size_t total_size = 0;
+
+    std::size_t linear_size = 0;
+
+    std::size_t circular_size = 0;
+
+    std::size_t linear_components = 0;
+
+    std::size_t circular_components = 0;
+
+    CircularType circular_type = CircularType::Euler;
+};
+
+#endif /* VECTORDESCRIPTION_H */
---
+++ src/BayesFilters/src/VectorDescription.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2016-2019 Istituto Italiano di Tecnologia (IIT)
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD 3-Clause license. See the accompanying LICENSE file for details.
+ */
+
+#include <BayesFilters/VectorDescription.h>
+
+using namespace bfl;
+
+
+VectorDescription::VectorDescription(const std::size_t linear_components, const std::size_t circular_components) :
+    VectorDescription(linear_components, circular_components, CircularType::Euler)
+{}
+
+
+VectorDescription::VectorDescription(const std::size_t linear_components, const std::size_t circular_components, const VectorDescription::CircularType& circular_type) :
+    total_size(linear_components + circular_components),
+    linear_size(linear_components),
+    circular_size(circular_components),
+    linear_components(linear_components),
+    circular_components(circular_components),
+    circular_type(circular_type)
+{
+    if (circular_type == CircularType::Quaternion)
+    {
+        circular_size = circular_components * 4;
+        total_size = linear_size + circular_size;
+    }
+}
